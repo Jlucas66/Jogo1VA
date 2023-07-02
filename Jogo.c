da0b8eb9d26b7ff1dea3bac3c9f2670f059e329a
João Lucas <jldearaujo1@gmail.com>
2023-07-02 15:14:25 -0300

    Versão final do jogo

--- Jogo.c
+++ Jogo.c
@@ -6,88 +6,142 @@ Jogo realizado para a 2° avaliação de aprendizagem da cadeira Introdução à
 Programação I do curso Bacharelado em Ciência da computação da UFRPE. Professor:
 George Moreno
 */
-// função static
-// definir a função para definir as posições aleatoriamente das paredes
-//
 
-//  bibliotecas
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include<windows.h>
-#include <time.h>
 #include <unistd.h>
 
-int i, j, t, tamanho = 40, lado = 40;
-int x, y, flag,  gameover = 0, score;
+int i, j, larg = 20, tam = 20;
+int gameover, score;
+int x, y, fruitx, fruity, flag;
 
-void posxy(int x, int y) { // função para definir a posição do boneco
- SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), (COORD){x, y});
-} 
-void posi(){
-  if(kbhit()){
-    switch(getch()){
-      case 'a':
-      flag = 1;
-      break;
-      case 's':
-      flag = 2;
-      break;
-      case 'd':
-      flag = 3;
-      break;
-      case 'w':
-      flag = 4;
-      break;
-      case 'x':
-      gameover = 1;
-      break;
-    }
-  }
-}
-void aleat(){
-  srand(time(NULL));
-   t = rand();
-   printf("%d", t % 40);
-
-}
-
-int main(void) {
-  aleat();
-while (!gameover)
+// Função para gerar a comida.
+void setup()
 {
+    gameover = 0;
 
-  for (i = 0; i < 50; i++) {
-    posxy(i + t, 0);
-    printf("%c", 219);
-    sleep(5);
-  }
-  for (i = 0; i < 20; i++) {
-    posxy(50, i + t);
-    printf("%c", 219);
-    sleep(5);
-  }
-  for (i = 50; i >= 0; i--) {
-    posxy(i + t, 20);
-    printf("%c", 219);
-    sleep(5);
-  }
-  for (i = 20; i >= 0; i--) {
-    posxy(0, i + t);
-    printf("%c", 219);
-    sleep(5);
-  }
-}
-  return 0;
+    x = larg / 2;
+    y = tam / 2;
+label1:
+    fruitx = rand() % 20;
+    if (fruitx == 0)
+        goto label1;
+label2:
+    fruity = rand() % 20;
+    if (fruity == 0)
+        goto label2;
+    score = 0;
 }
 
-/* script
-- criar as paredes e tentar fazer com que elas se movam aleatoriamente.
-- criar o boneco e fazer com que ele se mova de acordo com os comandos do
-usuário.
-- fazer com que, se o boneco tocar nas paredes, o jogo acabe.
-- colocar um tempo para que depois de alguns segundos as paredes comecem a
-fechar, tornando o jogo mais difícil.
-- tentar colocar um controlador de velocidade do boneco, para que o jogo fique
-mais interativo.
-*/
+// Função para desenhar o cenário
+void desenha()
+{
+    system("cls");
+    for (i = 0; i < larg; i++) {
+        for (j = 0; j < tam; j++) {
+            if (i == 0 || i == tam - 1
+                || j == 0
+                || j == larg - 1) {
+                printf("*");
+            }
+            else {
+                if (i == x && j == y)
+                    printf("<>");
+                else if (i == fruitx
+                         && j == fruity)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Sua pontuação é: = %d", score);
+    printf("\n");
+    printf("Aperte X para sair do jogo.");
+}
+
+// Função para registrar os comandos
+void input()
+{
+    if (kbhit()) {
+        switch (getch()) {
+        case 'a':
+            flag = 1;
+            break;
+        case 's':
+            flag = 2;
+            break;
+        case 'd':
+            flag = 3;
+            break;
+        case 'w':
+            flag = 4;
+            break;
+        case 'x':
+            gameover = 1;
+            break;
+        }
+    }
+}
+
+// Função para a lógica de todo o programa
+void logic()
+{
+    sleep(0.1);
+    switch (flag) {
+    case 1:
+        y--;
+        break;
+    case 2:
+        x++;
+        break;
+    case 3:
+        y++;
+        break;
+    case 4:
+        x--;
+        break;
+    default:
+        break;
+    }
+
+
+    if (x < 0 || x > larg
+        || y < 0 || y > tam)
+        gameover = 1;
+
+
+    if (x == fruitx && y == fruity) {
+    label3:
+        fruitx = rand() % 20;
+        if (fruitx == 0)
+            goto label3;
+
+    label4:
+        fruity = rand() % 20;
+        if (fruity == 0)
+            goto label4;
+        score += 10;
+    }
+}
+
+// Main do projeto, executa todos os comandos.
+void main()
+{
+    int m, n;
+
+
+    setup();
+
+
+    while (!gameover) {
+
+
+        desenha();
+        input();
+        logic();
+    }
+}
